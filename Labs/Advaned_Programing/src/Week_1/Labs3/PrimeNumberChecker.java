69fb72b3561515a748d122c0d4f71580ab926944
MinhPhu <minhphuzcod@gmail.com>
2024-03-08 20:22:54 +0700

    Labs1

---
+++ Labs/Advaned_Programing/src/Week_1/Labs3/PrimeNumberChecker.java
@@ -0,0 +1,30 @@
+package Week_1.Labs3;
+
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int num = scanner.nextInt();
+
+        boolean prim = isPrime(num);
+        System.out.println("The number: " + num + " is Prime : " + prim);
+        scanner.close();
+
+    }
+}
