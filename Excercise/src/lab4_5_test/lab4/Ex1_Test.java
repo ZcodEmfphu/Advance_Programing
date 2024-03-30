22e983c3c947f42c8744fdea6978b64901df73e7
MinhPhu <minhphuzcod@gmail.com>
2024-03-30 16:04:52 +0700

    Excercise 1 - Find number in Array

---
+++ Excercise/src/lab4_5_test/lab4/Ex1_Test.java
@@ -0,0 +1,62 @@
+package lab4_5_test.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lab4_5.lab4.*;
+
+public class Ex1_Test {
+
+	@Test
+	public void findMax() {
+		int[] arr1 = { 1, 4, 6, 8, 3 };
+		int[] arr2 = { -1, -4, -6, -8, 3 };
+		int[] arr3 = { 0 };
+
+		assertArrayEquals(new int[] { 4, 8 }, Ex1.findMax(arr1));
+		assertArrayEquals(new int[] { 5, 3 }, Ex1.findMax(arr2));
+		assertArrayEquals(new int[] { 1, 0 }, Ex1.findMax(arr3));
+	}
+
+	@Test
+	public void findMin() {
+		int[] arr1 = { 1, 4, 6, 8, 3 };
+		int[] arr2 = { -1, -4, -6, -8, 3 };
+		int[] arr3 = { 0 };
+
+		assertArrayEquals(new int[] { 1, 1 }, Ex1.findMin(arr1));
+		assertArrayEquals(new int[] { 4, -8 }, Ex1.findMin(arr2));
+		assertArrayEquals(new int[] { 1, 0 }, Ex1.findMin(arr3));
+	}
+
+	@Test
+	public void findNumber() {
+		int[] arr1 = { 1, 4, 6, 8, 3 };
+		int[] arr2 = { -1, -4, -6, -8, 3 };
+		int[] arr3 = { 0 };
+
+		assertEquals(5, Ex1.findNumber(arr1, 3));
+		assertEquals(4, Ex1.findNumber(arr2, -8));
+		assertEquals(1, Ex1.findNumber(arr3, 0));
+	}
+
+	@Test
+	public void sumOfArrays() {
+		int[] arr1 = { 1, 4, 6, 8, 3 };
+		int[] arr2 = { -1, -4, -6, -8, 3 };
+		int[] arr3 = { 0 };
+
+		assertEquals(22, Ex1.sumOfArrays(arr1));
+		assertEquals(-16, Ex1.sumOfArrays(arr2));
+		assertEquals(0, Ex1.sumOfArrays(arr3));
+	}
+
+}
+
+
+
+
+
+
+
