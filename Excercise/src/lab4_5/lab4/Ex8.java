dd5e6c769c7de0946a9ba8f82494a067b20aff37
MinhPhu <minhphuzcod@gmail.com>
2024-04-03 14:54:25 +0700

    Lab4

--- Excercise/src/lab4_5/lab4/Ex8.java
+++ Excercise/src/lab4_5/lab4/Ex8.java
@@ -2,4 +2,48 @@ package lab4_5.lab4;
 
 public class Ex8 {
 
+	public static int findMaxElement(int[][] matrix) {
+		int max = matrix[0][0];
+		for (int[] row : matrix) {
+			for (int element : row) {
+				if (element > max) {
+					max = element;
+				}
+			}
+		}
+		return max;
+	}
+
+	public static int findMinElement(int[][] matrix) {
+		int min = matrix[0][0];
+		for (int[] row : matrix) {
+			for (int element : row) {
+				if (element < min) {
+					min = element;
+				}
+			}
+		}
+		return min;
+	}
+
+	public static int[] findMinMax(int[][] matrix) {
+		int min = findMinElement(matrix);
+		int max = findMaxElement(matrix);
+
+		return new int[] { min, max };
+	}
+
+	public static int calculateSum(int[][] matrix) {
+		int sum = 0;
+		for (int[] row : matrix) {
+			for (int element : row) {
+				sum += element;
+			}
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+
+	}
 }
---
+++ Excercise/src/lab4_5_test/lab4/Ex9_Test.java
@@ -0,0 +1,26 @@
+package lab4_5_test.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lab4_5.lab4.*;
+
+public class Ex9_Test {
+
+	@Test
+	public void checkPalindrome() {
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		assertArrayEquals(new int[] { 6, 15, 24 }, Ex9.calculateRowSums(matrix));
+	}
+
+	@Test
+	public void checkAlternating() {
+		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[] rowSums = Ex9.calculateRowSums(matrix);
+
+		assertEquals(2, Ex9.findMaxRowIndex(rowSums));
+	}
+
+}
